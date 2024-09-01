6712f7bc9089fa41f8a84d528a895e2915d26eeb
PranoSA <pcadler@gmail.com>
2024-09-01 08:06:39 -0700

    Fixed Small Screen Problem

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
   };
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gray-100">
+    <main className="flex min-h-screen flex-col items-center justify-between p-4 lg:pd-24 bg-gray-100">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex flex-col space-y-8">
         <h1 className="text-3xl font-bold mb-8">
           Web Mercator Distance Calculator
@@ -40,39 +40,43 @@ export default function Home() {
           />
         </div>
 
-        <h1 className="text-3xl font-bold mb-8">Map Projection Views </h1>
+        <div className="w-full flex flex-col items-center space-y-4">
+          <h1 className="text-3xl font-bold mb-8">Map Projection Views </h1>
+        </div>
 
         {ProjectionLists.map((projection, index) => (
           <div
             key={projection.name}
             className="w-full flex flex-col items-center space-y-4"
           >
-            <h2 className="text-2xl font-semibold">{projection.name}</h2>
-            <Link
-              href={projection.link}
-              className="text-blue-500 hover:underline"
-            >
-              {projection.epsg_code}
-            </Link>
-            <button
-              onClick={() => toggleIndex(index)}
-              className="flex items-center space-x-2"
-            >
-              <span>
-                {openProjections[index] ? <BiChevronUp /> : <BiChevronDown />}
-              </span>
-              <span>View Description</span>
-            </button>
+            <div className="w-full flex flex-col items-center space-y-4">
+              <h2 className="text-2xl font-semibold">{projection.name}</h2>
+              <Link
+                href={projection.link}
+                className="text-blue-500 hover:underline"
+              >
+                {projection.epsg_code}
+              </Link>
+              <button
+                onClick={() => toggleIndex(index)}
+                className="flex items-center space-x-2"
+              >
+                <span>
+                  {openProjections[index] ? <BiChevronUp /> : <BiChevronDown />}
+                </span>
+                <span>View Description</span>
+              </button>
+            </div>
 
             {openProjections[index] && (
               <div className="w-full flex flex-row flex-wrap  items-center space-y-4">
-                <div className="w-full md:w-1/2 flex flex-col  items-center space-y-4 space-x-4 ">
+                <div className="w-full md:w-1/2 flex flex-col  items-center space-y-4 space-x-0 md:space-x-4 ">
                   {/* Title For Description */}
                   <h2 className="text-2xl font-semibold m-4">Description</h2>
                   <p>{projection.description}</p>
                 </div>
 
-                <div className="w-full flex md:w-1/2  flex-col items-center space-y-4 space-x-4">
+                <div className="w-full flex md:w-1/2  flex-col items-center space-y-4 space-x-0 md:space-x-4">
                   {/* Title For Map */}
                   <h2 className="text-2xl font-semibold">Map View</h2>
                   <Image
